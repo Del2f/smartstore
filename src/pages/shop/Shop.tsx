a7fc98f170bf5e6d962a851ced09b1f57e4d17d9
Del2f <thanks6@naver.com>
2024-02-10 16:24:26 +0900

    24-02-10 product mobile

--- src/pages/shop/Shop.tsx
+++ src/pages/shop/Shop.tsx
@@ -228,7 +228,7 @@ const NavHeightCover = styled.div<NavHeightType>`
     position: fixed;
     display: none;
     width: 100%;
-    height: calc(100dvh + 100px);
+    height: calc(100dvh + 200px);
     background: ${(props) => props.theme.navSubBG};
     z-index: 3;
 
